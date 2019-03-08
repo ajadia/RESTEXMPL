7c2233acb071cece16006b9173b7e7b95b125ce9
AN40008866 <AN40008866@D-113168594.wipro.com>
2019-03-08 20:43:31 +0530

    jaxrs update

--- src/main/java/com/wipro/mc/resource/MyApp.java
+++ src/main/java/com/wipro/mc/resource/MyApp.java
@@ -2,7 +2,9 @@ package com.wipro.mc.resource;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+/*
+ * http://localhost:8088/jaxrsapp/webapi/myresource
+ * */
 @ApplicationPath("webapi")
 public class MyApp extends Application{
 
